9a5d0280250ec08da41656fff8c76a63c30a6cee
Clark <35122143+smallbee3@users.noreply.github.com>
2022-07-12 19:16:16 +0900

    Test Google Login (Implicit flow > new > GAPI async/await)

--- mysite/frontend/urls.py
+++ mysite/frontend/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
 
 urlpatterns = [
     # https://developers.google.com/identity/oauth2/web/guides/migration-to-gis#the_new_way
     path('new1', TemplateView.as_view(template_name='implicitflow_new_1_gis_only.html')),
+    path('new2', views.New2TemplateView.as_view()),
 ]
--- mysite/frontend/views.py
+++ mysite/frontend/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class New2TemplateView(TemplateView):
+    template_name = 'implicitflow_new_2_gapi_async_await.html'
