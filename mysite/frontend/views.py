8f6fe79b808bd93ded73efa6bdad04bd0fb9858e
Clark <35122143+smallbee3@users.noreply.github.com>
2022-07-13 11:44:39 +0900

    Test Google Login (Implicit flow > new > GAPI callback)

--- mysite/frontend/views.py
+++ mysite/frontend/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class New2TemplateView(TemplateView):
     template_name = 'implicitflow_new_2_gapi_async_await.html'
+
+
+def new3(request):
+    return render(request, 'implicitflow_new_3_gapi_callback.html')
