f900f93f3d6c56a42108094d07be4d12fed66e77
Clark <35122143+smallbee3@users.noreply.github.com>
2022-07-14 19:22:53 +0900

    Test Google Login (Implicit flow > old ways > GIS Popup UX)

--- mysite/frontend/urls.py
+++ mysite/frontend/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('new2', views.New2TemplateView.as_view()),
     path('new3', views.new3),
     path('new4', views.New4View.as_view()),
+
+    # https://developers.google.com/identity/oauth2/web/guides/migration-to-gis#the_old_way
+    path('old1', TemplateView.as_view(template_name='implicitflow_old_1_gapi_client_library.html')),
+    path('old2', TemplateView.as_view(template_name='implicitflow_old_2_js_client_library.html')),
 ]
